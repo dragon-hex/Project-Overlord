b58753ba4230b78be335602d5d6cf7a46ddfeaee
dragon-hex <infinityuser0101@gmail.com>
2022-03-02 10:52:03 -0300

    new utilities! colors in linux!

--- ord/utils.py
+++ ord/utils.py
@@ -3,22 +3,35 @@ import sys
 import random
 import json
 
+# NOTE: in-case you want to have colorful outputs
+# on the debug! this only supports linux :/
+SUPPORT_COLOR_TERM = True
+
 # class: debugReporter
 class debugReporter:
     def __init__(self, enabled):
-        self.outputs = [sys.stdout]
-        self.enabled = enabled
-        self.location = 'unknown'
+        self.outputs    = [sys.stdout]
+        self.enabled    = enabled
+        self.location   = 'unknown'
+        self.counter    = 0
     
     def write(self, string: str):
         if self.enabled:
             for output in self.outputs:
-                try:    output.write(("[%s] debug: " % self.location) + string + "\n")
+                try:
+                    if SUPPORT_COLOR_TERM:    
+                        output.write(
+                            ("\033[1;31m%0.8d\033[0m \033[1;33m[%s]\033[0m \033[1;38mdebug: %s\033[0m\n") % 
+                            (self.counter, self.location, string)
+                        )
+                    else:
+                        output.write("%0.8d [%s] debug: %s\n" % (self.counter, self.location, string))
                 except Exception as E: 
                     # NOTE: this will break the debug.
                     self.outputs.remove(output)
                     return
-
+        # set the counter here.
+        self.counter += 1
 #
 def getPythonVersion():
     """return the python version."""
