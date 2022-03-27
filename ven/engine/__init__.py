914916d9605e8bda4cdd53339ddebce3f1945407
dragon-hex <infinityuser0101@gmail.com>
2022-03-27 16:23:48 -0300

    new module organization header.

---
+++ ven/engine/__init__.py
@@ -0,0 +1,7 @@
+"""
+Engine: everything related with the first level of interaction, the engine
+controls the world, and every GUI, the core provides the content loader +
+window system and core manager.
+"""
+from .core import *
+from .engine import *
